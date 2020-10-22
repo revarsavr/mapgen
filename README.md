9edfc7e3277860a9d8a4c713c5d607b9c2183111
revarsavr <61622849+revarsavr@users.noreply.github.com>
2020-10-22 18:07:46 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -2,16 +2,4 @@
 
 mapgen es un generador de mapas válidos aleatorios. Genera un mapa (en el directorio actual) llamado "rand.cub", que va a ser siempre válido, y como mínimo de dimensiones 10x10.
 
-Uso: 
-
-1) gcc -Wall -Werror -Wextra mapgen.c -o mapgen
-
-2) ./mapgen
-
-3) ./cub3D rand.cub
-
-cub3D debería ser el nombre de tu programa para visualizar mapas (mediante RayCasting).
-
-Nota: 
-
-si tu cub3D ya visualiza texturas, asegúrate de cambiar "./path_to_the..." en mapgen.c a sus directorios correspondientes, y de cambiar el número de bytes a escribir (el último argumento de la función write).
+Uso: ./mapgen && ./cub3D rand.cub
--- mapgen.c
+++ mapgen.c
@@ -3,7 +3,19 @@
 #include <time.h>
 #include <unistd.h>
 
-int main(void)
+void	cabecera(int fd)
+{
+    write(fd, "R 1920 1080 \n", 13);
+    write(fd, "NO ./path_to_the_north_texture\n",31);
+	write(fd, "SO ./path_to_the_south_texture\n",31);
+	write(fd, "WE ./path_to_the_west_texture\n",30);
+	write(fd, "EA ./path_to_the_east_texture\n",30);
+	write(fd, "S ./path_to_the_sprite_texture\n",31);
+	write(fd, "F 25,25,25\n",11);
+	write(fd, "C 0,126,233\n\n", 13);
+}
+
+int		main(int argc, char **argv)
 {
 	int		x;
 	int		y;
@@ -15,19 +27,27 @@ int main(void)
 	char	*dirs;
 	int		posx;
 	int		posy;
-
+	int		d;
+        
+        
+	if (argc > 2)
+		return (0);
+	d = 50;
+	if (argc == 2)
+		d = atoi(argv[1]);
 	dirs = "NESW";
 	fd = open("rand.cub", O_RDWR | O_CREAT | O_TRUNC, 0644);
 	srand(time(NULL));
 	w = 10 + rand() % 10;
 	h = 10 + rand() % 10;
 	i = 0;
-	write(fd, "R 1920 1080\nNO ./path_to_the_north_texture\nSO ./path_to_the_south_texture\nWE ./path_to_the_west_texture\nEA ./path_to_the_east_texture\nS ./path_to_the_sprite_texture\nF 25,25,25\nC 0,126,233\n\n", 189);
-	while (i++ < w + 2)
-		write(fd, "1", 1);
-	write(fd, "\n", 1);
 	posx = rand() % h;
 	posy = rand() % w;
+	
+	cabecera(fd);
+	while (i++ < w + 2)
+	write(fd, "1", 1);
+	write(fd, "\n", 1);
 	x = 0;
 	while (x < h)
 	{
@@ -35,9 +55,11 @@ int main(void)
 		write(fd, "1", 1);
 		while (y < w)
 		{
-			c = '0' + rand() % 2;
+                        c = '0';
+			if (rand() % 100 < d)
+                            c = '1';
 			if (x == posx && y == posy)
-				c = dirs[rand() % 4];
+                            c = dirs[rand() % 4];
 			write(fd, &c, 1);
 			y++;
 		}
